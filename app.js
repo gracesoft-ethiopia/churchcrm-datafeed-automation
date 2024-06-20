6879cd0caa64abc7e594e176031a73a9d8ed5def
Wuletaw Wonte <wuletawwonte@gmail.com>
2024-06-20 20:05:25 +0300

    save select field of gender using Select class

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ import fs from 'fs';
 import csv from 'csv-parser';
 import { Browser, Builder } from 'selenium-webdriver';
 import dotenv from 'dotenv';
-import loginUser, { exitBrowser } from './loginuser.js';
+import loginUser, { exitBrowser, saveMember } from './loginuser.js';
 
 dotenv.config();
 
@@ -18,10 +18,11 @@ await loginUser(driver, `${SYSTEM_BASE_URL}/PersonEditor`);
 
 fs.createReadStream('assets/members.csv')
   .pipe(csv())
-  .on('data', (data) => {
+  .on('data', async (data) => {
     members.push(data);
   })
-  .on('end', () => {
+  .on('end', async () => {
+    await saveMember(driver, members[0]);
     console.log('CSV file successfully processed');
   });
 
--- loginuser.js
+++ loginuser.js
@@ -1,4 +1,4 @@
-import { By } from 'selenium-webdriver';
+import { By, Select } from 'selenium-webdriver';
 
 export default async function loginUser(driver, loggedinurl) {
   const loginForm = await driver.findElement(By.name('LoginForm'));
@@ -16,4 +16,25 @@ export default async function loginUser(driver, loggedinurl) {
 export async function exitBrowser(driver) {
   await driver.sleep(1000);
   await driver.quit();
+}
+
+export async function saveMember(driver, member) {
+  const gender = await driver.findElement(By.name('Gender'));
+  const firstName = await driver.findElement(By.name('FirstName'));
+  const middleName = await driver.findElement(By.name('MiddleName'));
+  const lastName = await driver.findElement(By.name('LastName'));
+  const saveButton = await driver.findElement(By.name('PersonSubmitAndAdd'));
+
+  // select object creation for select fields
+  const genderSelect = new Select(gender);
+
+  console.log(member);
+
+  await genderSelect.selectByVisibleText(member['Gender']);
+  await firstName.sendKeys(member['First name']);
+  await middleName.sendKeys(member['Middle Name']);
+  await lastName.sendKeys(member['Last Name']);
+
+  // await saveButton.click();
+  await driver.sleep(1000);
 }
