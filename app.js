4c2a91cde7c70bd695c8634bf06ead97b3ad3d96
Wuletaw Wonte <wuletawwonte@gmail.com>
2024-06-20 17:23:54 +0300

    add a separate user login module

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ import fs from 'fs';
 import csv from 'csv-parser';
 import { Browser, Builder, By, Key } from 'selenium-webdriver';
 import dotenv from 'dotenv';
+import loginUser from './loginuser.js';
 
 dotenv.config();
 
@@ -10,21 +11,18 @@ const SYSTEM_BASE_URL = 'https://crm.amskhc.org';
 const members = [];
 
 let driver = await new Builder().forBrowser(Browser.CHROME).build();
+
 await driver.get(SYSTEM_BASE_URL);
+
+await loginUser(driver, `${SYSTEM_BASE_URL}/PersonEditor`);
 
 fs.createReadStream('assets/members.csv')
   .pipe(csv())
   .on('data', (data) => {
     members.push(data);
   })
-  .on('end', async () => {
-    const loginForm = await driver.findElement(By.name('LoginForm'));
-    const username = await driver.findElement(By.name('User'));
-    const password = await driver.findElement(By.name('Password'));
-
-    await username.sendKeys(process.env.CRMUSER);
-    await password.sendKeys(process.env.CRMPWD);
-    await loginForm.submit();
+  .on('end', () => {
+    console.log('CSV file successfully processed');
   });
 
 await setTimeout(() => {
---
+++ loginuser.js
@@ -0,0 +1,14 @@
+import { By } from 'selenium-webdriver';
+
+export default async function loginUser(driver, loggedinurl) {
+  const loginForm = await driver.findElement(By.name('LoginForm'));
+  const username = await driver.findElement(By.name('User'));
+  const password = await driver.findElement(By.name('Password'));
+
+  await username.sendKeys(process.env.CRMUSER);
+  await password.sendKeys(process.env.CRMPWD);
+  await loginForm.submit();
+
+  await driver.get(loggedinurl);
+  await driver.sleep(1000);
+}
